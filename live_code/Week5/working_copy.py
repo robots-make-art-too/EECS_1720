c6ee4ba4b0981d09b4627cd393212b7f044f8d4d
MissCrispenCakes <MissCrispenCakes@gmail.com>
2022-02-08 15:49:00 -0500

    inital code pt2

--- live_code/Week5/working_copy.py
+++ live_code/Week5/working_copy.py
@@ -17,33 +17,42 @@ colour = None
 
 
 
-
 # 1. how to setup: setup(), draw()
 def setup():
-  size(600,600)
-  smooth()
-  global numRings, ring
+  size(600,600);
+  smooth();
+  global numRings, ring;
 
   for i in range(numRings): 
-    colour = color(random(255), random(255), random(255))
-    ring[i] = Ring(0, 0, 0, False, colour) # Create each object
+    colour = color(random(255), random(255), random(255));
+    ring[i] = Ring(0, 0, 0, False, colour); # Create each object
+
+
 
 
 def draw():
-  global numRings, rings
+  global numRings, rings;
+  background(251,128,114)
+
+  for i in range(numRings):
+    ring[i].grow();
+    ring[i].display();
 
 
 
 def mousePressed():
-  global numRings, rings
+ 
+ global numRings, rings;
   # when we click we want to activate a ring at the location of the cursor
   # so our xpos = mouseX
   # and the ypos = mouseY
-  ring[currentRing].start(mouseX, mouseY)
+  ring[currentRing].start(mouseX, mouseY);
 
-  currentRing += 1
+  currentRing += 1;
   if (currentRing >= numRings):
-    currentRing = 0
+    currentRing = 0;
+
+
 
 
 class Ring(object):
@@ -51,11 +60,11 @@ class Ring(object):
   # do we want our rings to have?
 	# 1. initialize
   def __init__(self, x, y, diameter, on, colour):
-    self.x = x
-    self.y = y
-    self.diameter = diameter
-    self.on = False
-    self.colour = colour
+    self.x = x;
+    self.y = y;
+    self.diameter = diameter;
+    self.on = False;
+    self.colour = colour;
 
   # 2. start
 	def start(self, xpos, ypos):
@@ -67,9 +76,19 @@ class Ring(object):
   # 3. grow
   def grow(self):
 
+    if (self.on == True):
+      self.diameter += 1.5
+      if (self.diameter > 400):
+        self.on = False
 
   # 4. display
   def display(self):
+
+    if (self.on == True):
+      noFill()
+      strokeWeight(4)
+      stroke(self.colour) #stroke(255, 153)
+      ellipse(self.x, self.y, self.diameter, self.diameter)
 
 
 # note: we need this object to know about
