51d602ef7a3007d8404f114e9b48aa8b74dbe82e
MissCrispenCakes <MissCrispenCakes@gmail.com>
2022-02-08 14:55:55 -0500

    inital code

---
+++ live_code/Week5/working_copy.py
@@ -0,0 +1,41 @@
+# Python - processing.py
+
+#
+# 2. rings:
+# a way to count the rings
+
+numRings = 40
+
+#	a way to store the rings
+
+ring = [0]*numRings
+
+#	keep track of which rings
+
+currentRing = 0
+
+# arrays start at a value of 0
+
+
+
+
+
+# 1. how to setup: setup(), draw()
+def setup():
+
+
+
+
+def draw():
+
+
+
+
+def mousePressed():
+
+
+
+
+
+class Ring(object):
+		
