ca6c0e322e133daf28e5d52cccc0e18a050154fc
MissCrispenCakes <MissCrispenCakes@gmail.com>
2022-02-09 13:29:51 -0500

    lab 2

--- live_code/lab02/example.py
+++ live_code/lab02/example.py
@@ -46,21 +46,71 @@ video = cv2.VideoCapture("filename.ext")
 # this integer could also be a file name 
 # or the integer indicates a camera attached and if more than one which one
 
+
 # We need to check if camera isOpened()
 if (video.isOpened() == False):
   print("Error reading video file")
 
+# We need to set resolution -> convert them from float to integer
+frame_width = int(video.get(3))
+frame_height = int(video.get(4))
+
+size = (frame_width, frame_height)
 
 
-# short confirmation and loop to access camera, 
-# grab by frame an image
-# save the image, (and the video)
-# add to an API function (we don't build these)
+# did we detect a video?
+# save if we did
+result = cv2.VideoWriter('vid/output.avi',
+                         cv2.VideoWriter_fourcc('M','J','P','G'),
+                         10, size)
+
+while(video.isOpened()):
+
+  # try to read if video is available
+  # res, image = cam.read()
+  ret, frame = video.read()
+
+  # short confirmation and loop to access camera, 
+
+  if ret == True:
+
+    # Write the frame into the
+    # file 'output.avi' ^^ from before
+    result.write(frame)
+
+    # Display the frame
+    # save it 
+    cv2.imshow('Frame', frame)
+    cv2.imwrite('img/Frame.png', frame)
+
+    # grab by frame an image
+    # save the image, (and the video)
+    # add to an API function (we don't build these)
 
     statusMessage = "Hi I am an image!"
     api.update_status_with_media(statusMessage,"img/Frame.png")
 
+
+    # Press q on keyboard
+    # to stop the process and grab a photo!
+    # (from the last frame or instance of the cv2.imwrite() & cv2.imShow())
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+      break
+
+  # Break the loop condition
+  else:
+      break
+
 # post (you could grab, read, etc)
 
-# close
+# close# dont forget to release() 
+# cam.release()
+video.release()
+result.release()
 
+# Closes all the frames
+cv2.destroyAllWindows()
+# # or specifically
+# cv2.destroyWindow('Frame')
+
+print("The video was successfully saved")
