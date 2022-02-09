8e0b5c6196e4f0c096623998e2adcb8747e0ccd3
MissCrispenCakes <MissCrispenCakes@gmail.com>
2022-02-09 13:01:48 -0500

    lab 2

---
+++ live_code/lab02/example.py
@@ -0,0 +1,31 @@
+# libraries
+
+import cv2 # specific for accessing the camera and handling this type of live media
+
+import time # so we can pause for the camera to warm up or initate 
+import sys # this is system - in case we want to access system
+import imutils # think of my as a utility library
+from PIL import Image # image handling library
+import os # to access the OS so saving files or direct command line etc
+
+
+import tweepy #  twitter API 
+
+
+
+
+# authentification
+
+# load my authetifciation keys/tokens from a file
+
+# my choice of media is video/images direct from my camera/webcam
+
+# short confirmation and loop to access camera, 
+# grab by frame an image
+# save the image, (and the video)
+# add to an API function (we don't build these)
+
+# post (you could grab, read, etc)
+
+# close
+
