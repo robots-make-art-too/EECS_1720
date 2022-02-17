8d1b30d4fe9c6398bfb73443007b1419d7b8f92e
MissCrispenCakes <10444589+MissCrispenCakes@users.noreply.github.com>
2022-02-17 16:11:39 -0500

    server

--- live_code/Feb17/python/server.py
+++ live_code/Feb17/python/server.py
@@ -4,6 +4,12 @@ import socketserver
 PORT = 3030 #8080, 8887, 8898,
 Handler = http.server.SimpleHTTPRequestHandler
 
-with socketserver.TCPServer() as httpd:
-	print("Hi you are at PORT: ", PORT)
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+  print("Hi you are at PORT: ", PORT)
   httpd.serve_forever()
+
+
+# to run>: python3 server.py
+# might have to install the imports
+# pip/pip3 install http.server, socketserver
+# 
